5914cc1154e58f17c7a00c98d56c1b9ee0240eee
Dennis <hall.dennis@gmail.com>
2019-07-21 07:15:55 -0400

    * move data to separate file * show color groups on board * update font to one that's closer * "GO" space styling

--- src/Space.js
+++ src/Space.js
@@ -2,16 +2,30 @@ import React from 'react';
 
 function Space (props) {
   var space = props.space;
+  var isCorner = /\b(go|jail|free)\b/.test(space.type);
+  var isRailroad = (space.name || '').match(/railroad/i);
+  var isUtility = (space.name || '').match(/electric|water/i);
+  var hasPropertyGroupColor = !isRailroad && !isUtility && space.type === 'property';
   return (
     <div
       className={[
         'space',
         space.type.replace(/ /g, '-'),
-        (space.group || '').replace(/ /g, '-') +
-        (/\b(go|jail|free)\b/.test(space.type) ? 'corner' : ''),
+        (space.group || '') +
+        (isCorner ? 'space--corner' : ''),
       ].join(' ')}
     >
-      {space.name || space.type}
+      {hasPropertyGroupColor &&
+        <div
+          className={'space__color'}
+          style={{backgroundColor: (space.group || '')}}
+        >
+          {/* intentionally empty */}
+        </div>
+      }
+      <div className={'space__name'}>
+        {space.name || space.type}
+      </div>
     </div>
   );
 }
---
+++ src/boardSpacesData.js
@@ -0,0 +1,430 @@
+var boardSpacesData = [
+  {
+    type: 'go',
+  },
+  {
+    type: 'property',
+    name: 'Mediter-ranean Avenue',
+    group: 'purple',
+    price: 60,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'community chest',
+  },
+  {
+    type: 'property',
+    name: 'Baltic Avenue',
+    group: 'purple',
+    price: 60,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'income tax',
+  },
+  {
+    type: 'property',
+    name: 'Reading Railroad',
+    group: 'railroad',
+    price: 200,
+    rents: [
+      25,
+      50,
+      100,
+      200,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Oriental Avenue',
+    group: 'lightblue',
+    price: 100,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'chance',
+  },
+  {
+    type: 'property',
+    name: 'Vermont Avenue',
+    group: 'lightblue',
+    price: 100,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Connecticut Avenue',
+    group: 'lightblue',
+    price: 100,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'jail',
+  },
+  {
+    type: 'property',
+    name: 'St. Charles Place',
+    group: 'MediumVioletRed',
+    price: 140,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Electric Company',
+    group: 'utility',
+    price: 150,
+  },
+  {
+    type: 'property',
+    name: 'States Avenue',
+    group: 'MediumVioletRed',
+    price: 140,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Virginia Avenue',
+    group: 'MediumVioletRed',
+    price: 160,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Pennsylvania Railroad',
+    group: 'railroad',
+    price: 200,
+    rents: [
+      25,
+      50,
+      100,
+      200,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'St. James Place',
+    group: 'orange',
+    price: 180,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'community chest',
+  },
+  {
+    type: 'property',
+    name: 'Tennessee Avenue',
+    group: 'orange',
+    price: 180,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'New York Avenue',
+    group: 'orange',
+    price: 200,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'free parking',
+  },
+  {
+    type: 'property',
+    name: 'Kentucky Avenue',
+    group: 'red',
+    price: 220,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: "chance",
+  },
+  {
+    type: 'property',
+    name: 'Indiana Avenue',
+    group: 'red',
+    price: 220,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Illinois Avenue',
+    group: 'red',
+    price: 240,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'B & O Railroad',
+    group: 'railroad',
+    price: 200,
+    rents: [
+      25,
+      50,
+      100,
+      200,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Atlantic Avenue',
+    group: 'yellow',
+    price: 260,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Ventnor Avenue',
+    group: 'yellow',
+    price: 260,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Water Works',
+    group: 'utility',
+    price: 150,
+  },
+  {
+    type: 'property',
+    name: 'Marvin Gardens',
+    group: 'yellow',
+    price: 280,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'go to jail',
+  },
+  {
+    type: 'property',
+    name: 'Pacific Avenue',
+    group: 'green',
+    price: 300,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'North Carolina Avenue',
+    group: 'green',
+    price: 300,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: "community chest",
+  },
+  {
+    type: 'property',
+    name: 'Pennsylvania Avenue',
+    group: 'green',
+    price: 320,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'property',
+    name: 'Shortline Railroad',
+    group: 'railroad',
+    price: 200,
+    rents: [
+      25,
+      50,
+      100,
+      200,
+    ]
+  },
+  {
+    type: "chance",
+  },
+  {
+    type: 'property',
+    name: 'Park Place',
+    group: 'darkblue',
+    price: 350,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+  {
+    type: 'luxury tax',
+  },
+  {
+    type: 'property',
+    name: 'Boardwalk',
+    group: 'darkblue',
+    price: 400,
+    housePrice: 50,
+    rents: [
+      4,
+      20,
+      60,
+      200,
+      300,
+      400,
+    ]
+  },
+];
+
+export default boardSpacesData;
