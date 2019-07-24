a1a533e76a974e89bb1cf2db5fb50cfa9f7922f5
Dennis <hall.dennis@gmail.com>
2019-07-24 12:14:10 -0400

    DRY edges - tenSpaces()

--- src/App.js
+++ src/App.js
@@ -6,49 +6,31 @@ import boardSpacesData from './boardSpacesData';
 
 import './App.scss';
 
-function App() {
+function App () {
+
+  var tenSpacesStart = -10;
+
+  function tenSpaces () {
+    tenSpacesStart += 10;
+    return boardSpacesData.slice(tenSpacesStart, tenSpacesStart + 10).map((space, i) => (
+      <Space
+        key={i}
+        space={space}
+      >
+      </Space>
+    ))
+  }
+
   return (
     <div className="App">
       <div className="edge edge--bottom">
-        {boardSpacesData.map((space, i) => (
-          i < 10 ?
-            <Space
-              key={i}
-              space={space}
-            >
-            </Space>
-            : ''
-        ))}
+        {tenSpaces()}
         <div className="edge edge--left">
-          {boardSpacesData.map((space, i) => (
-            i > 9 && i < 20 ?
-              <Space
-                key={i}
-                space={space}
-              >
-              </Space>
-              : ''
-          ))}
+          {tenSpaces()}
           <div className="edge edge--top">
-            {boardSpacesData.map((space, i) => (
-              i > 19 && i < 30 ?
-                <Space
-                  key={i}
-                  space={space}
-                >
-                </Space>
-                : ''
-            ))}
+            {tenSpaces()}
             <div className="edge edge--right">
-              {boardSpacesData.map((space, i) => (
-                i > 29 ?
-                  <Space
-                    key={i}
-                    space={space}
-                  >
-                  </Space>
-                  : ''
-              ))}
+              {tenSpaces()}
             </div>
           </div>
         </div>
